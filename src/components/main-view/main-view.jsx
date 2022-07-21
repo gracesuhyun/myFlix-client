1adb716ce16ce8220b655d28313316c44a6364a5
gracesuhyun <gracesuhyun@gmail.com>
2022-07-21 01:49:58 -0400

    add a registration view and login view

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import axios from 'axios';
+
+import { LoginView } from '../login-view/login-view';
 import { MovieCard } from '../movie-card/movie-card';
 import { MovieView } from '../movie-view/movie-view';
 
@@ -7,13 +10,22 @@ export class MainView extends React.Component {
   constructor() {
     super();
     this.state = {
-      movies: [
-        { _id: 1, Title: 'Soul', Description: 'Joe, a jazz player, travels to another realm to help someone find their passion, he soon discovers what it means to have soul.', ImagePath: 'https://thenavigatornews.com/wp-content/uploads/2021/02/Soul-by-Pixar-1.jpeg'},
-        { _id: 2, Title: 'Luca', Description: 'Set in a beautiful seaside town on the Italian Riviera, the original animated feature is a coming-of-age story about one young boy experiencing an unforgettable summer filled with gelato, pasta and endless scooter rides.', ImagePath: 'https://m.media-amazon.com/images/M/MV5BZTQyNTU0MDktYTFkYi00ZjNhLWE2ODctMzBkM2U1ZTk3YTMzXkEyXkFqcGdeQXVyNTI4MzE4MDU@._V1_.jpg'},
-        { _id: 3, Title: 'Tenet', Description: 'A secret agent embarks on a dangerous, time-bending mission to prevent the start of World War III.', ImagePath: 'https://m.media-amazon.com/images/M/MV5BYzg0NGM2NjAtNmIxOC00MDJmLTg5ZmYtYzM0MTE4NWE2NzlhXkEyXkFqcGdeQXVyMTA4NjE0NjEy._V1_.jpg'}
-      ],
-      selectedMovie: null
+      movies: [],
+      selectedMovie: null,
+      user: null
     };
+  }
+
+  componentDidMount() {
+    axios.get('https://gracean-movies.herokuapp.com/movies')
+      .then(response => {
+        this.setState({
+          movies:response.data
+        });
+      })
+      .catch(error => {
+        console.log(error);
+      });
   }
 
   setSelectedMovie(newSelectedMovie) {
@@ -22,11 +34,20 @@ export class MainView extends React.Component {
     });
   }
 
+  onLoggedIn(user) {
+    this.setState({
+      user
+    });
+  }
+
   render() {
-    const {movies, selectedMovie} = this.state;
-    
+    const {movies, selectedMovie, user} = this.state;
+    /* If there is no user, the LoginView is rendered. If there is a user logged in, the user details are *passed as a prop to the LoginView*/
+    if (!user) return <LoginView onLoggedIn={user => this.onLoggedIn(user)} />;
+
+    // Before the movies have been loaded
     if (movies.length === 0)
-      return <div className="main-view">The list is empty!</div>;
+      return <div className="main-view" />;
 
       return (
         <div className="main-view">
--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,6 +1,21 @@
 import React from 'react';
 
 export class MovieView extends React.Component {
+
+    keypressCallback(event) {
+        console.log(event.key);
+    }
+
+    componentDidMount() {
+        document.addEventListener('keypress', event => {
+            console.log(event.key);
+        });
+    }
+
+    componentWillUnmount() {
+        document.removeEventListener('keypress', this.keypressCallback);
+      }
+
     render() {
 
         const {movie, onBackClick} = this.props;
@@ -9,12 +24,17 @@ export class MovieView extends React.Component {
             <div className="movie-view">
 
                 <div className="movie-poster">
-                    <img src={movie.ImagePath} />
+                    <img crossOrigin="anonymous" height="300" src={movie.ImagePath} />
                 </div>
 
                 <div className="movie-title">
                     <span className="label">Title: </span>
                     <span className="value">{movie.Title}</span>
+                </div>
+
+                <div className="movie-genre">
+                    <span className="label">Genre: </span>
+                    <span className="value">{movie.Genre.name}</span>
                 </div>
 
                 <div className="movie-description">
